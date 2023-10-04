45cc115191ffa7a4a3e9be7e597b8266b33d401a
Santiago Castiello <santiagocdo@gmail.com>
2023-10-04 14:12:03 -0400

    output file changed manually and analysis script in process to visualize

--- analysis.R
+++ analysis.R
@@ -3,12 +3,11 @@ rm(list=ls())
 
 # where is the data
 # dataLocation <- "C:/Users/owner/Desktop/bayesianGhost/data/"
-dataLocation <- "data/"
+dataLocation <- "data/behaviour/"
 
 # get csv files names
 dataFiles <- list.files(dataLocation)
-qualtric <- read.csv(paste0(dataLocation,dataFiles[1]))
-dataFiles <- dataFiles[-1]
+qualtric <- read.csv("data/qualtrics/bayesianGhost_October 3, 2023_14.23.csv")
 
 # read all subjects 
 db_raw <- lapply(paste0(dataLocation,dataFiles), read.csv)
@@ -17,11 +16,14 @@ for (i in 1:length(dataFiles)) {
   # duration in minutes
   duration <- (max(temp$time_elapsed)/1000)/60
   # filter relevant columns
-  temp <- temp[!is.na(temp$index),c("stim","index","workerId","noise","action",
-                                    "scramble","communicative","response")]
-  temp <- data.frame(temp[seq(1,nrow(temp),by=2),],
-                     confidence=temp$response[seq(2,nrow(temp),by=2)])
-  colnames(temp)[c(-3:-7)] <- c("video","trial","detection","confidence")
+  temp <- temp[!is.na(temp$index),c("stim","index","workerId","interview_date",
+                                    "noise","action","scramble","communicative",
+                                    "response","confidence")]
+  # temp <- data.frame(temp[seq(1,nrow(temp),by=2),],
+  #                    confidence=temp$response[seq(2,nrow(temp),by=2)])
+  temp$response <- c("",temp$response[1:(nrow(temp)-1)])
+  temp <- temp[!is.na(temp$confidence),]
+  colnames(temp)[c(-3:-8)] <- c("video","trial","detection","confidence")
   temp$detection <- ifelse(temp$detection=="yes",1,0)
   temp$confidence <- as.integer(temp$confidence)
   temp$correct <- ifelse((1-temp$scramble)==temp$detection,1,0)
@@ -34,13 +36,14 @@ for (i in 1:length(dataFiles)) {
 
 
 
+# # # # Basic raw Viualization # # # #
 if (!require(ggplot2)) {install.packages("ggplot2")}; library(ggplot2)
 table(db$scramble)
 table(db$communicative)
 table(db$detection)
-table(db$confidence)
+hist(db$confidence)
 ggplot(db, aes(x=scramble,y=detection)) + stat_summary()
-ggplot(db, aes(x=communicative,y=detection,col=as.factor(scr))) + stat_summary()
+ggplot(db, aes(x=communicative,y=detection,col=as.factor(scramble))) + stat_summary()
 ggplot(db, aes(x=scramble,y=confidence)) + stat_summary()
 ggplot(db, aes(x=detection,y=confidence)) + stat_summary()
 ggplot(db, aes(x=confidence,y=detection)) + stat_summary()
