0ffa93a148c17fe9f968a7058115bad6c4b250e6
Santiago Castiello <santiagocdo@gmail.com>
2023-10-05 17:38:26 -0400

    first visualization done! API of qualtrics needs to extract data in string and not values form

--- function.R
+++ function.R
@@ -11,3 +11,46 @@ rawPooler <- function (data_location = NULL) {
   return(raw_data_dataframe)
 }
 
+scoreQuestionnaire <- function(qualtrics) {
+  
+  qualtrics <- as.data.frame(qualtrics)
+  
+  # # # # Revised Green Paranoid Thought Scale # # # #
+  cols_rgpts <- colnames(qualtrics)[grepl("rgpts_",colnames(qualtrics))]
+  # reference (A)
+  referential <- qualtrics[,grepl("rgpts_referential",colnames(qualtrics)),]
+  referential <- referential-1
+  qualtrics$referential <- rowSums(referential)
+  qualtrics$referential - qualtrics$referential - ncol(referential)
+  qualtrics$referential_dich <- ifelse(qualtrics$referential <= 9,"average","elevated")
+  # persecution (B)
+  persecution <- qualtrics[,grepl("rgpts_persecution",colnames(qualtrics)),]
+  persecution <- persecution-1
+  qualtrics$persecution <- rowSums(persecution)
+  qualtrics$persecution - qualtrics$persecution - ncol(persecution)
+  qualtrics$persecution_dich <- ifelse(qualtrics$persecution <= 5,"average","elevated")
+  # paranoia (A+B)
+  qualtrics$paranoia <- qualtrics$referential + qualtrics$persecution
+  # at least one subscale elevated
+  qualtrics$paranoia_dich <- ifelse(qualtrics$referential_dich == "elevated" |
+                                          qualtrics$persecution_dich == "elevated",
+                                        "elevated","average")
+  
+  # # # # Beliefs in Purpose of Events # # # #
+  # Lindeman, M., & Aarnio, K. (2007). Superstitious, magical, and paranormal beliefs: An integrative model. Journal of research in Personality, 41(4), 731-744.
+  cols_bpe <- colnames(qualtrics)[grepl("bpe_",colnames(qualtrics))]
+  cols_bpe <- cols_bpe[grepl("_1",cols_bpe)]
+  # sum all items  
+  qualtrics$bpe <- rowSums(qualtrics[,cols_bpe])/length(cols_bpe)
+  
+  # # # # Peters Delusion Inventory # # # #
+  cols_pdi <- colnames(qualtrics)[grepl("pdi_Q2#1",colnames(qualtrics))]
+  # get pdi 
+  pdi <- qualtrics[,cols_pdi]
+  pdi <- as.data.frame(ifelse(pdi == 1,1,0))
+  # sum all items  
+  qualtrics$pdi <- rowSums(pdi,na.rm=T)/length(cols_pdi)
+  
+  # return
+  return(qualtrics)
+}
--- main.R
+++ main.R
@@ -5,6 +5,10 @@ source("api/getTask.R")
 qualtrics <- getSurvey("ghost") # name is found in surveyIds.R
 qualtrics$survey_duration <- qualtrics$EndDate - qualtrics$StartDate
 
+source("function.R")
+# score questionnaires
+qualtrics <- scoreQuestionnaire(qualtrics)
+
 # get merged behaviour 
 behaviour <- getTask("ghost", "workerId") # name is found in mongo collections
 
@@ -15,32 +19,43 @@ subject <- unique(behaviour$workerId)
 for (i in 1:length(subject)) {
   temp <- behaviour[behaviour$workerId == subject[i],]
   # task duration in minutes
-  task_duration <- (max(temp$time_elapsed)/1000)/60
+  # task_duration <- (max(temp$time_elapsed)/1000)/60
+  task_duration <- NA
+  questTemp <- qualtrics[qualtrics$workerId == subject[i],]
   # survey duration in minutes
-  survey_duration <- qualtrics$survey_duration[qualtrics$workerId == subject[i]]
+  survey_duration <- questTemp$survey_duration
   # filter relevant columns
-  temp <- temp[,c("stim","index","workerId","interview_date","noise","action",
+  temp <- temp[,c("video","trial","workerId","interview_date","noise","action",
                   "scramble","communicative","response","confidence")]
+  # temp <- temp[,c("stim","index","workerId","interview_date","noise","action",
+  #                 "scramble","communicative","response","confidence")]
   # temp <- data.frame(temp[seq(1,nrow(temp),by=2),],
   #                    confidence=temp$response[seq(2,nrow(temp),by=2)])
-  temp$response <- c(NA,temp$response[1:(nrow(temp)-1)])
-  temp <- temp[!is.na(temp$confidence),]
-  colnames(temp) <- c("video","trial","workerId","interview_date","noise","action",
-                      "scramble","communicative","response","confidence")
+  # temp$response <- c(NA,temp$response[1:(nrow(temp)-1)])
+  # temp <- temp[!is.na(temp$confidence),]
+  # colnames(temp) <- c("video","trial","workerId","interview_date","noise","action",
+  #                     "scramble","communicative","response","confidence")
   temp$detection <- ifelse(temp$response=="yes",1,0)
-  temp$correct <- ifelse((1-temp$scramble)==temp$detection,1,0)
+  # temp$correct <- ifelse((1-temp$scramble)==temp$detection,1,0)
   if (i == 1) {
-    db <- temp
+    db <- data.frame(temp,bpe=questTemp$bpe,
+                     referential_dich=questTemp$referential_dich,
+                     persecution_dich=questTemp$persecution_dich,
+                     paranoia_dich=questTemp$paranoia_dich)
     genChar <- data.frame(workerId=subject[i],interview_date=temp$interview_date[1],
                           task_duration=task_duration,
                           survey_duration=survey_duration)
   } else {
-    db <- rbind(db,temp)
+    db <- rbind(db,data.frame(temp,bpe=questTemp$bpe,
+                              referential_dich=questTemp$referential_dich,
+                              persecution_dich=questTemp$persecution_dich,
+                              paranoia_dich=questTemp$paranoia_dich))
     genChar <- rbind(genChar,data.frame(workerId=subject[i],interview_date=temp$interview_date[1],
                                         task_duration=task_duration,
                                         survey_duration=survey_duration))
   }
 }
+
 
 
 
@@ -50,7 +65,45 @@ table(db$scramble)
 table(db$communicative)
 table(db$detection)
 hist(db$confidence)
-ggplot(db, aes(x=scramble,y=detection)) + stat_summary()
+
+db$correct <- ifelse((1-db$scramble) == db$detection,1,0)
+
+db$trial_type <- as.factor(paste0(db$scramble,db$communicative))
+levels(db$trial_type) = c("Signal_IND","Signal_COM","Noise_IND","Noise_COM")
+
+p1 <- ggplot(db, aes(x=trial_type,y=correct,col=paranoia_dich)) +
+  labs(title="Detection Agent B",x="Trial Type",y="p(correct)",
+       col="Paranoia:") +
+  stat_summary(aes(group=workerId), alpha=0.1, size=0.5,
+               position = position_dodge(0.2), geom="point") +
+  stat_summary(position = position_dodge(0.2)) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 30,hjust=1)) 
+
+p2 <- ggplot(db, aes(x=trial_type,y=detection,col=paranoia_dich)) +
+  labs(title="Detection Agent B",x="Trial Type",y="p(detection)",
+       col="Paranoia:") +
+  stat_summary(aes(group=workerId), alpha=0.1, size=0.5,
+               position = position_dodge(0.2), geom="point") +
+  stat_summary(position = position_dodge(0.2)) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 30,hjust=1)) 
+
+p3 <- ggplot(db, aes(x=trial_type,y=confidence,col=paranoia_dich)) +
+  labs(title="Confidence (1 to 5)",x="Trial Type",y="Confidence",
+       col="Paranoia:") +
+  stat_summary(aes(group=workerId), alpha=0.1, size=0.5,
+               position = position_dodge(0.2), geom="point") +
+  stat_summary(position = position_dodge(0.2)) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 30,hjust=1)) 
+
+library(ggpubr)
+ggpubr::ggarrange(p2,p3, common.legend = T)
+
+
+
+ggplot(db, aes(x=scramble,y=detection,col=paranoia_dich)) + stat_summary()
 ggplot(db, aes(x=communicative,y=detection,col=as.factor(scramble))) + stat_summary()
 ggplot(db, aes(x=scramble,y=confidence)) + stat_summary()
 ggplot(db, aes(x=detection,y=confidence)) + stat_summary()
